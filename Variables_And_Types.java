c1dc10fec0010acefd4c66e7d17ddca5de4f3f81
randerson112358 <randerson112358@users.noreply.github.com>
2017-08-31 23:26:29 -0400

    Create Variables_And_Types.java

---
+++ Variables_And_Types.java
@@ -0,0 +1,42 @@
+
+public class Variables_And_Types {
+
+	public static void main(String[] args) {
+		// This is a comment
+		/* This is another comment */
+		
+		/*DATA TYPES*/
+		//byte 
+		//short 
+		//int
+		//long
+		//float
+		//double
+		//char
+		//boolean
+		
+		//NUMBERS
+		//<data type> <variable name> ;
+		int myNumber;
+		myNumber = 10;
+		//<data type> <variable name> = <number value>;
+		int anotherNumber = 11;
+		
+		double aDouble = 4;
+		
+		//CHARACTER
+		//<data type> <variable name> = '<value>';
+		char aCharacter = '$';
+		String s1 = new String("This is a string");
+		
+		//Print to the console screen
+		System.out.println(myNumber);
+		System.out.println(anotherNumber);
+		System.out.println(aDouble);
+		System.out.println(aCharacter);
+		System.out.println(s1);
+		
+
+	}
+
+}
